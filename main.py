01d3524fc10de10ff54f2e4ec0642c867f921f84
Cybernecromancer <johannespaavola@gmail.com>
2025-05-20 17:18:49 +0300

    modified main.py to read the frankenstein.txt

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    result = get_book_text("bookbot/books/frankenstein.txt")
+    print(result)
+
+main()
